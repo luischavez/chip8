534b90474436e5e4c7bf7ce2e811fb2226286ca1
Luis <luis_13_30_4@hotmail.com>
2015-03-15 21:40:55 -0700

    Se cambio la licencia en las cabezeras.

--- src/main/java/mx/uach/fing/chip8/OPCode.java
+++ src/main/java/mx/uach/fing/chip8/OPCode.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2015 Your Organisation
+/* 
+ * Copyright (C) 2015 UACH
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
